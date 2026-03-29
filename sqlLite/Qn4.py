5017b1e3531a4830711390f85cccc40febafe767
Om Official <omstha71@gmail.com>
2026-03-29 15:30:34 +0545

    Edited some in sqlLite

--- sqlLite/Qn4.py
+++ sqlLite/Qn4.py
@@ -1,7 +1,14 @@
+# To FEtch data from database and print them.
 import sqlite3
-
+name = input("enter name to search: ")
 conn = sqlite3.connect("student.db")
 cursor = conn.cursor()
 
-cursor.execute("SELECT * FROM students LIMIT 20")
-print(cursor.fetchall())
+cursor.execute("SELECT * FROM students LIMIT 10")
+data = cursor.fetchall()
+
+cursor.execute("SELECT * FROM students WHERE name = ?", (name,))
+data = cursor.fetchall()
+
+for row in data:
+    print(row)
